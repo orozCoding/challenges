b2302b765a6668984a355d47ddae8c02babbab98
orozcoding <orozcoding@gmail.com>
2021-10-17 02:50:37 -0400

    added staircase solved

--- js/staircase.js
+++ js/staircase.js
@@ -1,21 +1,27 @@
 function staircase(n) {
-
-  let i = 0;
   let string = "";
+  let i = 0;
   let arr = [];
 
   while (i < n) {
-    string += " ";
+    arr.push(" ");
     i++;
   }
+  console.log(arr);
+  string = arr.join("");
+  console.log(string);
+  /*
+  console.log("entrando al segundo loop");*/
   
-  console.log(i);
+  i -= 1;
 
- // while (i > 0) {
-   // string.replace(string.charAt(i), "#");
-    //console.log(string);
-    //i--;
-  //}
+  while (i >= 0) {
+    //console.log(string.charAt(i))
+    arr.splice(i, 1, "#");
+    string = arr.join("");
+    console.log(string);
+    i--;  
+  }
 }
 
 console.log(staircase(6));
